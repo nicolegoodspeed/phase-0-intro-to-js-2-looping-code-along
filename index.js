520a958bff7e4f6f147a905544553324314b82ae
Nicole <nicole.goodspeed18@gmail.com>
2023-10-11 09:52:39 -0400

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+function writeCards(cards) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < cards.length; i++) {
+      const message = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const cards = ["Guadalupe", "Ollie", "Aki"];
+  console.log(writeCards(cards));
+
+
+  function countDown(number) {
+    let countDown = number;
+    while (countDown >= 0) {
+      console.log(countDown);
+      countDown--;
+    }
+  
+    return countDown;
+  }
+  
+  countDown(10);
